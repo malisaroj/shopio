41c7760e7eec86909929109646b64b92d48057ee
Deepesh Mali <deepeshmali@Deepeshs-MacBook-Air.local>
2019-07-26 16:21:45 +0545

    changes

---
+++ components/productBox.vue
@@ -0,0 +1,54 @@
+<template>
+    <div>
+        <b-row class="no-gutters">
+            <b-col lg="3" md="6" v-for="(p,i) in products" :key="i">
+                <div class="product-box">
+                    <img :src="p.image" :alt="p.name">
+                    <h2>{{p.name}}</h2>
+                    <p>{{p.info}}</p>
+                    <div class="price-row">
+                        <div class="stars">0 0 0 0 0</div>
+                        <div class="price">{{p.price}}</div>
+                    </div>
+                </div>
+            </b-col>
+        </b-row>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        components: {},
+        props: ['products'],
+        name: "productBox"
+    }
+</script>
+
+<style scoped>
+    .product-box p{
+font-size: 13px;
+        color: #989898;
+    }
+    .product-box h2{
+        color: #4c4c4c;
+        font-size: 16px;
+    }
+    .product-box img{
+        margin-bottom: 20px;
+
+    }
+    .product-box {
+        background-color: rgb(255, 255, 255);
+        box-shadow: 0px 0px 25px 0px rgba(226, 233, 255, 0.65);
+        min-height: 310px;
+        padding: 10px;
+        margin-bottom: 30px;
+        border-radius: 6px;
+    }
+    .price-row {
+        display: flex;
+        justify-content: space-around;
+        margin-top: 20px;
+    }
+</style>
