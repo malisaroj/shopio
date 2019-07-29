7a9fa0408cc7ee690f64376236949473be038d90
malisaroj <malisaroj@icloud.com>
2019-07-29 14:00:59 +0545

    Add buy now button

--- components/Ads.vue
+++ components/Ads.vue
@@ -1,13 +1,10 @@
 <template>
   <div class="container">
     <div class="row">
-      <div
-        v-for="(item, index) in ads"
-        :key="index"
-        class="col-3 ad-container"
-      >
+      <div v-for="(item, index) in ads" :key="index" class="col-3 ad-container">
         <div class="product-img">
           <img :src="item.image" :alt="item.title" />
+          <button class="buynow" type="submit" v-if="item.id===3">buy now</button>
         </div>
       </div>
     </div>
@@ -22,19 +19,19 @@ export default {
         {
           id: 1,
           title: "Gaming Console",
-          image: require("../assets/images/xbox.png"),
+          image: require("../assets/images/xbox.png")
         },
 
         {
           id: 2,
           title: "Running Shoe",
-          image: require("../assets/images/nike.png"),
+          image: require("../assets/images/nike.png")
         },
 
         {
           id: 3,
           title: "LeEco Le2  32GB",
-          image: require("../assets/images/leca.png"),
+          image: require("../assets/images/leca.png")
         }
       ]
     };
@@ -43,16 +40,26 @@ export default {
 </script>
 
 <style>
-
 .ad-container {
-    margin: 40px !important;
+  margin: 40px !important;
 }
 
 .product-img {
-    max-width: 100%;
-    height: auto;
+  max-width: 100%;
+  height: auto;
 }
 
+.buynow {
+  position: absolute;
+  bottom: 75px;
+  left: 65px;
+  width: 83px;
+  height: 33px;
+  background-color: rgb(51, 51, 51);
+  font-size: 12px;
+  color: rgb(227, 238, 109);
+  line-height: 1.5em;
+  text-align: center;
+  text-transform: capitalize;
+}
 </style>
-
-
